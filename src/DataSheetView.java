7ab6e7ba474150339653453b3e57bb55a5ace3e5
kimYejin <kimphoby0125@gmail.com>
2024-11-01 15:46:31 +0900

    feat : DataSheetView(ConcreteObservers) 구현

--- src/DataSheetView.java
+++ src/DataSheetView.java
@@ -1,2 +1,28 @@
-public class DataSheetView {
+import java.util.List;
+
+public class DataSheetView implements Observer{
+
+    private ScoreRecord scoreRecord;
+    private int viewCount;
+
+    public DataSheetView(ScoreRecord scoreRecord, int viewCount) {
+        this.scoreRecord = scoreRecord;
+        this.viewCount = viewCount;
+
+    }
+
+    @Override
+    public void update() {
+        List<Integer> currentScores = scoreRecord.getScoreRecord();
+        displayScores(currentScores);
+    }
+
+    public void displayScores(List<Integer> record) {
+        System.out.print("List of Scores: ");
+        for(int i=0; i<record.size() && i<viewCount; i++) {
+            System.out.print(record.get(i) + " ");
+        }
+        System.out.println();
+    }
+
 }
