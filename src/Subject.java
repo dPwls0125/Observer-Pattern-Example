8a54e5921c2b2c993018ded7117630ee842248e8
kimYejin <kimphoby0125@gmail.com>
2024-11-01 15:36:16 +0900

    feat : Subject 추상 클래스 구현

--- src/Subject.java
+++ src/Subject.java
@@ -1,2 +1,25 @@
-public class Subject {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Subject {
+
+    // 추상화된 통보대상(관찰자) 목록
+    private List<Observer> observers = new ArrayList<Observer>();
+
+    // 통보 대상(Observer) 추가
+    private void attach(Observer observer) {
+        observers.add(observer);
+    }
+    // 통보 대상(Observer) 제거
+    private void detach(Observer observer) {
+        observers.remove(observer);
+    }
+
+    // 각 통보 대상(Observer)에게 변경을 통보
+    private void notifyObservers() {
+        for (Observer o : observers) {
+            o.update();
+        }
+    }
+
 }
