4cad32e94aacfeddac2c7cc7cdb76ef357a3210a
kimYejin <kimphoby0125@gmail.com>
2024-11-01 15:40:34 +0900

    feat : ConcreteSubject 구현

--- src/ScoreRecord.java
+++ src/ScoreRecord.java
@@ -1,2 +1,19 @@
-public class ScoreRecord {
+import java.util.ArrayList;
+import java.util.List;
+
+// Concrete Subject
+public class ScoreRecord extends Subject{
+
+    // 점수 저장 클래스
+    List<Integer> scores = new ArrayList<Integer>();
+
+    // 새로운 점수를 추가 (상태 변경)
+    public void addScore(int score) {
+        scores.add(score); // scores 리스트에 score 추가 => 리스트 변경 발생
+        notifyObservers(); // 데이터가 변경되었음을 각 통보 대상(Observer)에게 통보
+    }
+
+    public List<Integer> getScoreRecord() {
+        return scores;
+    }
 }
--- src/Subject.java
+++ src/Subject.java
@@ -4,19 +4,19 @@ import java.util.List;
 public abstract class Subject {
 
     // 추상화된 통보대상(관찰자) 목록
-    private List<Observer> observers = new ArrayList<Observer>();
+    public List<Observer> observers = new ArrayList<Observer>();
 
     // 통보 대상(Observer) 추가
-    private void attach(Observer observer) {
+    public void attach(Observer observer) {
         observers.add(observer);
     }
     // 통보 대상(Observer) 제거
-    private void detach(Observer observer) {
+    public void detach(Observer observer) {
         observers.remove(observer);
     }
 
     // 각 통보 대상(Observer)에게 변경을 통보
-    private void notifyObservers() {
+    public void notifyObservers() {
         for (Observer o : observers) {
             o.update();
         }
