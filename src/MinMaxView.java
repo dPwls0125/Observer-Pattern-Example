3357beffd110663b78eebc1727865e8ea1241623
kimYejin <kimphoby0125@gmail.com>
2024-11-01 15:49:11 +0900

    feat : MinMaxView(ConcreteObservers) 구현

--- src/MinMaxView.java
+++ src/MinMaxView.java
@@ -1,2 +1,26 @@
+import java.util.Collections;
+import java.util.List;
+
 public class MinMaxView {
+
+    private ScoreRecord scoreRecord;
+
+    public MinMaxView(ScoreRecord scoreRecord) {
+        this.scoreRecord = scoreRecord;
+    }
+
+    // 점수의 변경을 통보받음
+    public void update() {
+        List<Integer> record = scoreRecord.getScoreRecord();
+        displayMinMax(record);
+    }
+
+    // 최소값고 최대값을 출력함
+    public void displayMinMax(List<Integer> record) {
+        int min = Collections.min(record, null);
+        int max = Collections.max(record, null);
+        System.out.println("Min: " + min + ", Max: " + max);
+    }
+
+
 }
